07b346c3f9bd09ea1aa04eb641d68031b0aeb1a5
Beysemi Emirhan BAYRAK <54868884+beysemi@users.noreply.github.com>
2021-02-07 20:57:15 +0300

    Create harfduyarliligi.js

---
+++ harfduyarliligi.js
@@ -0,0 +1,13 @@
+/*
+    JavaScipt büyük küçük harf duyarlılığı olan bir dildir.
+*/
+
+var Degisken = "Bu bir baş harfi büyük tanımlanan değişken. -----> Degisken";
+var degisken = "Bu bir baş harfi küçük olarak tanımlanan değişken. -----> degisken";
+
+document.write(Degisken + '<br>');
+document.write(degisken);
+
+/*
+  Görüldüğü üzere JS büyük küçük harfe duyarlı bir dildir.
+*/
