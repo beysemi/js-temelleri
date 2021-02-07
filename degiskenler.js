f1a64adc33cbfb034b86856749a78e586197b26f
Beysemi Emirhan BAYRAK <54868884+beysemi@users.noreply.github.com>
2021-02-07 20:54:21 +0300

    Create degiskenler.js

---
+++ degiskenler.js
@@ -0,0 +1,11 @@
+  var varDegisken = "bu bir değişkendir.";
+
+  /*ES6 ile kazanılan değişken özellikleri*/
+  let letDegisken = "bu da bir değişkendir fakat kullanılan block da tekrar tanımlanabilir yada değiştirilebilir.";
+  const sabitDegisken = "bu bir sabit değişkendir tekrar tanımlanması mümükün değildir."
+  
+var sayi1 = 5; // 1. sayıyı tanımladık.
+var sayi2 = 2; // 2. sayıyı tanımladık.
+var sonuc = sayi1 + sayi2 ; //sonuçun depolanacağı  degişkenini oluşturduk ve toplama işlemini ona atadık.
+document.write(sonuc); // sonucu yazdırdık.
+
